4187424cb6d31b48c8681b2c835fcce81a0461f3
Suriya Kodeswaran <suriyakode@gmail.com>
2017-09-24 21:34:00 -0500

    client parse args

--- http_client.cpp
+++ http_client.cpp
@@ -11,12 +11,14 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <iostream>
 
 #include <arpa/inet.h>
 
 #define PORT "3490" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
+using namespace std;
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -28,6 +30,47 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+string parse_args(char * argv[]){
+	string port = "80";
+	int colon_pos = 0, slash_pos = 0, hostname_end = 0;;
+	string filename = "", hostname = "";
+	string arg = string(argv[1]);
+	if (arg.compare(0, 7, "http://") != 0){
+		fprintf(stderr, "parse_args: use http:// in arg");
+		exit(1);
+	}
+
+	if ((slash_pos = arg.find("/", 7)) > 0){
+		filename.append(arg.substr(slash_pos, arg.length())); 
+		if ((colon_pos = arg.find(":", 7)) > 7){
+			port = arg.substr(colon_pos + 1, slash_pos - colon_pos - 1).c_str();
+			hostname_end = colon_pos;
+		}
+		else {
+			hostname_end = slash_pos;
+		}	
+	}
+	else{
+		fprintf(stderr, "parse_args: no file specified");
+		exit(1);
+	}
+	
+	hostname.append(arg.substr(7, hostname_end - 7));
+
+	cout << "hostname: " << hostname << endl;
+	cout << "port: " << port << endl;
+	cout << "filename: " << filename << endl;
+	
+	string output = "";
+	output.append("GET " + filename + " HTTP/1.1\r\n"); 
+	output.append("User-Agent: MP1/1\r\n");
+	output.append("Accept: /\r\n");
+	output.append("Accept-Encoding: ide\r\n");
+	output.append("Host: " + hostname + ":" + port + "\r\n");
+	
+	return output;
+}
+
 int main(int argc, char *argv[])
 {
 	int sockfd, numbytes;  
@@ -37,10 +80,11 @@ int main(int argc, char *argv[])
 	char s[INET6_ADDRSTRLEN];
 
 	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
+	    fprintf(stderr, "usage: client hostname[:port]/path/to/file\n");
 	    exit(1);
 	}
-
+	string header = parse_args(argv);
+	cout << "header:\n" << header << endl;
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
@@ -77,6 +121,10 @@ int main(int argc, char *argv[])
 	printf("client: connecting to %s\n", s);
 
 	freeaddrinfo(servinfo); // all done with this structure
+	
+	if (send(sockfd, header.c_str(), header.length(), 0) == -1){
+		perror("send");
+	}
 
 	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
 	    perror("recv");
@@ -91,4 +139,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
