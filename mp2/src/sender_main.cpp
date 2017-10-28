5f1530c9b851c5a940f22b95eae466d2a8334687
Suriya Kodeswaran <suriyakode@Suriyas-MacBook-Pro.local>
2017-10-28 15:36:37 -0500

    starting up MP

--- mp2/src/sender_main.cpp
+++ mp2/src/sender_main.cpp
@@ -5,6 +5,15 @@
  * Created on 
  */
 
+//user added
+#include <iostream>
+#include <string>
+#include <sstream> 
+#include <fstream>
+#include <streambuf>
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,8 +29,16 @@
 #include <string.h>
 #include <sys/time.h>
 
+#define MTU 1500 
+
+using namespace std;
+
 struct sockaddr_in si_other;
 int s, slen;
+
+int send_base = 0;
+int cwnd = 1;
+
 
 void diep(char *s) {
     perror(s);
@@ -31,19 +48,25 @@ void diep(char *s) {
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
     //Open the file
-    FILE *fp;
-    fp = fopen(filename, "rb");
-    if (fp == NULL) {
-        printf("Could not open file to send.");
-        exit(1);
-    }
+    char buf[1500];
+   	ifstream inFile;
+   	inFile.open(filename);
+
+   	if(!inFile){
+   		diep( (char *) "file");
+   		exit(1);
+   	}
+
+   	stringstream file_stream;
+    file_stream << inFile.rdbuf();
+    string file_string = file_stream.str();
 
 	/* Determine how many bytes to transfer */
 
     slen = sizeof (si_other);
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        diep("socket");
+        diep( (char *) "socket");
 
     memset((char *) &si_other, 0, sizeof (si_other));
     si_other.sin_family = AF_INET;
@@ -52,12 +75,26 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
-
-
+    string packet;
+    int transferred_bytes = 0, sentBytes = 0;
+    while (transferred_bytes != bytesToTransfer) {
+	    int packets_in_window = 0;
+	    packet = file_string.substr(transferred_bytes, MTU);
+	    while (packet.length() && packets_in_window != cwnd) {
+		    
+		    if ((sentBytes = sendto(s, packet.c_str(), packet.length(), 0, (struct sockaddr *) &si_other, slen) == -1)) {
+				diep( (char *) "send");
+				exit(1);
+			}
+			transferred_bytes += sentBytes;
+			packets_in_window++;
+		}
+	}
 	/* Send data and receive acknowledgements on s*/
 
     printf("Closing the socket\n");
     close(s);
+    inFile.close();
     return;
 
 }
